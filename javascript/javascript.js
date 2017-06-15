2f6176c2ab9383397b4f1b366a28440a98bfb969
Sean Medford <sean.medford@outlook.com>
2017-06-15 19:50:48 -0400

    argh
    argh

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,73 +1,142 @@
 $(document).ready(function() {
 
-    // Initialize Firebase
-    var config = {
-    apiKey: "AIzaSyCj_AZGEOgcPxJj6-qlIAjeE7o_uIGpoxY",
-    authDomain: "pawsandlook-c56d5.firebaseapp.com",
-    databaseURL: "https://pawsandlook-c56d5.firebaseio.com",
-    projectId: "pawsandlook-c56d5",
-    storageBucket: "pawsandlook-c56d5.appspot.com",
-    messagingSenderId: "555976363149"
-    };
-  
-    firebase.initializeApp(config);
+	var config = {
+	    apiKey: "AIzaSyCj_AZGEOgcPxJj6-qlIAjeE7o_uIGpoxY",
+	    authDomain: "pawsandlook-c56d5.firebaseapp.com",
+	    databaseURL: "https://pawsandlook-c56d5.firebaseio.com",
+	    projectId: "pawsandlook-c56d5",
+	    storageBucket: "pawsandlook-c56d5.appspot.com",
+	    messagingSenderId: "555976363149"
+  	};
 
-    var database = firebase.database();
+  	firebase.initializeApp(config);
 
-    //click handler for AJAX to pull pet info from the API
-	$("#submit").on("click", function() {
+	var database = firebase.database();
 
-		event.preventDefault();
-		
-		$("petDiv").empty();
+	//setting cat dog images to an array
+	var choiceArr = [
+    { image: "assets/images/cat.png",
+	  alt: "cat" }, 
+    { image: "assets/images/dog.png",
+      alt: "dog" }
+	];
 
-		var zipInput = $("#zip").val();
-		var userZip = '&location=' + zipInput;
-		var pfApiUrl = "http://api.petfinder.com/pet.find?format=json&key=f411ff9725d287d4138503a0c95030a6&count=1&output=basic"
-		var queryURL = pfApiUrl + userZip;
-		console.log(queryURL);
+	//declaring global variables for the pet favorites
+	var name = "";
+	var age = 0;
+	var photo = "";
+	var petTable = $(".table");
+	var newBody;
+	var newTr;
+	var imageTd;
+	var nameTd;
+	var sexTd;
 
-		$.ajax({
-			url: queryURL,
-			dataType: "jsonp",
-			method: "GET"
-		}).done(function(response) {
 
-			console.log(response);
+	//function to get the cat/dog choices to page
+	function images () {
+		for (var i = 0; i < choiceArr.length; i++) {
+			var choiceDisplay = $("<img>");
+			choiceDisplay.addClass("choice-image");
+			choiceDisplay.attr("src", choiceArr[i].image);
+			choiceDisplay.attr("alt", choiceArr[i].alt);
+			$("#pet-choices").prepend(choiceDisplay);
+		}
+	};
 
-		    var petInfo = {
-			animal: response.petfinder.pets.pet.animal.$t,
-			breed: response.petfinder.pets.pet.breeds.breed.$t,
-			sex: response.petfinder.pets.pet.sex.$t,
-			name: response.petfinder.pets.pet.name.$t,
-			age: response.petfinder.pets.pet.age.$t,
-			//address1: response.petfinder.pets.pet.contact.address1.$t,
-			//address2: response.petfinder.pets.pet.contact.address2.$t,
-			city: response.petfinder.pets.pet.contact.city.$t,
-			state: response.petfinder.pets.pet.contact.state.$t,
-			zip: response.petfinder.pets.pet.contact.zip.$t,
-			//phone: response.petfinder.pets.pet.contact.phone.$t,
-			email: response.petfinder.pets.pet.contact.email.$t,
-			photo: response.petfinder.pets.pet.media.photos.photo[2].$t
-		    }
-		
-		    console.log(petInfo);
+	//calling function  for cat dog pics
+	images();
 
-			$("#petDiv").append("<p>" + response.petfinder.pets.pet.animal.$t + "</p>");
-			$("#petDiv").append("<p>" + response.petfinder.pets.pet.breeds.breed.$t + "</p>");
-			$("#petDiv").append("<p>" + response.petfinder.pets.pet.sex.$t + "</p>");
-			$("#petDiv").append("<p>" + response.petfinder.pets.pet.name.$t + "</p>");
-			$("#petDiv").append("<p>" + response.petfinder.pets.pet.age.$t + "</p>");
-			$("#petDiv").append("<li>" + response.petfinder.pets.pet.contact.address1.$t + "</li>");
-			$("#petDiv").append("<li>" + response.petfinder.pets.pet.contact.address2.$t + "</li>");
-			$("#petDiv").append("<li>" + response.petfinder.pets.pet.contact.city.$t + "</li>");
-			$("#petDiv").append("<li>" + response.petfinder.pets.pet.contact.state.$t + "</li>");
-			$("#petDiv").append("<li>" + response.petfinder.pets.pet.contact.zip.$t + "</li>");
-			$("#petDiv").append("<li>" + response.petfinder.pets.pet.contact.phone.$t + "</li>");
-			$("#petDiv").append("<li>" + response.petfinder.pets.pet.contact.email.$t + "</li>");
-			$("#petDiv").append("<img src=" + response.petfinder.pets.pet.media.photos.photo[2].$t + "/>");
-
-			database.ref().push(petInfo);	
-		});
+	//function for flip animations
+		$('.fliper-btn').click(function(){
+		$('.flip').find('.card').toggleClass('flipped');
 	});
+
+	  database.ref().on("child_added", function(childSnapshot) {
+
+
+      data = childSnapshot.val();
+      var dataArr = Object.keys(data);
+
+      		// fucntion to append users save searches to page
+      		function displayFavorite() {
+
+      		//creates classes for table
+	      	newBody = $("<tbody class = 'pet-group'>");
+			newTr = $("<tr class = 'pet-row'>");
+	        imageTd = $("<td class = 'pet-image'>");
+	        nameTd = $("<td class = 'pet-name'>");
+	        sexTd = $("<td class = 'pet-sex'>");
+
+	        //creates a containter for thumbnail
+            var thumbDisplay = $("<img>");
+            thumbDisplay.addClass("pet-image");
+			thumbDisplay.attr("src", data.photo);
+   
+   			//adding data to classes
+            imageTd.append(thumbDisplay);
+            nameTd.append(data.name);
+            sexTd.append(data.sex);
+
+            //appending data to page
+ 			nameTd.addClass("center");
+ 			sexTd.addClass("center");
+            newTr.append(imageTd);
+            newTr.append(nameTd);
+            newTr.append(sexTd);
+            newBody.append(newTr);
+
+            //gives data attributes to each pet group
+      		newBody.attr("data-image", data.photo);
+      		newBody.attr("data-name", data.name);
+      		newBody.attr("data-sex", data.sex);
+      		newBody.attr("data-age", data.age);
+      		newBody.attr("data-breed", data.breed);
+
+      		//and finally writing every single thing to the HTML
+            petTable.append(newBody);  
+
+
+		};
+        
+        displayFavorite();
+
+        
+        //event for when a user clicks on a pet group
+        $(".pet-group").on("click", function () {
+        		//calling the data attributes for the group
+				imageValue = $(this).attr("data-image");
+				nameValue = $(this).attr("data-name");
+	      		sexValue = $(this).attr("data-sex");
+	      		ageValue = $(this).attr("data-age");
+	      		breedValue = $(this).attr("data-breed");
+
+	      		//emptying out the div and displaying the user's selection
+				$(".main-login").empty();
+				$('html, body').animate({ scrollTop: 150 }, 'fast');
+				$(".main-login").html("<a href= 'favorite-pets.html'><i class='fa fa-arrow-left'</a>");
+				var imageDisplay = $("<img>");
+				imageDisplay.addClass("image");
+				imageDisplay.attr("src", imageValue);
+				$(".main-login").append("<div class=image>");
+				$(".main-login").append("<div class=name>");
+				$(".main-login").append("<div class=sex>");
+				$(".main-login").append("<div class=age>");
+				$(".main-login").append("<div class=breed>");
+				$(".image").append(imageDisplay);
+				$(".name").append(nameValue);
+				$(".sex").append(sexValue);
+				$(".age").append(ageValue);
+				$(".breed").append(breedValue);
+		});
+
+
+    },  
+
+      function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+      });      
+
+
+
 });
