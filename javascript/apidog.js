5d8b3c4ae054d55465604fea92a333611f938b4e
ryan-judy <rj113909@gmail.com>
2017-06-18 15:20:45 -0400

    Ryan updates - not pushing to FB

--- javascript/apidog.js
+++ javascript/apidog.js
@@ -1,163 +1,78 @@
 $(document).ready(function() {
-	var map;
-	$('#view-map').hide();
+        var config = {
+        apiKey: "AIzaSyCj_AZGEOgcPxJj6-qlIAjeE7o_uIGpoxY",
+        authDomain: "pawsandlook-c56d5.firebaseapp.com",
+        databaseURL: "https://pawsandlook-c56d5.firebaseio.com",
+        projectId: "pawsandlook-c56d5",
+        storageBucket: "pawsandlook-c56d5.appspot.com",
+        messagingSenderId: "555976363149"
+    };
 
-	$("#negative").on("click", function() {
-	
-	var key = "&key=F411ff9725d287d4138503a0c95030a6"
-	var AnimURL = "http://api.petfinder.com/pet.find?format=json"
+    firebase.initializeApp(config);
 
-// variables for user input and petfinder api output
-	var zip = "20882"
-	var locationQuery = "&location=" + zip;
-	var locationID = "";
-	var locLat = 0;
-	var locLong = 0;
+    var database = firebase.database();
+    var map;
+    var mapURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+    var locationID = "";
+    var mapAddress = "";
+    var globalLat;
+    var globalLng;
+    var key = "&key=F411ff9725d287d4138503a0c95030a6"
+    var AnimURL = "http://api.petfinder.com/pet.find?format=json"
+    var animLocLat;
+    var animnLocLong;
+    var zip = "";
+    var catDogSelect = "dog";
+    var catDogAnim = "";
+    var ageSelect = "";
+    var nameID = "";
+    var ageID = "";
+    var sexSelect = "";
+    var sexID = "";
+    var breedSelect = "";
+    var breedID = "";
+    var sizeSelect = "";
+    var sizeID = "";
+    var respAbrev = "";
+    var addr = "";
+    var city = "";
+    var state = "";
+    var zipID = "";
+    var phoneID = "";
+    var emailID = "";
+    var photo;
+    var loop = false;
+    $('#map').hide();
+    // $('#view-map').hide();
+    // console.log(google);
+    	DisplayAnimal();
+        
+        Geolocator();
+        // map = new google.maps.Map(document.getElementById('map'), {
+        
+    $('#negative').on('click', function() {
+        DisplayAnimal();
+        // $('#view-map').show();
+        
+    })
 
-	var catDogSelect = "dog";
-	var catDogQuery = "&animal=";
-	var catDogAnim = "";
+    $('#positive').on('click', function() {
+    	FireBaseFunction();
+    	DisplayAnimal();
+    })
+    $('#view-map').on('click', function() {
+        MakeMap();
+        $('#map').show();
+    })
+    
+                
+	//------------Functions---------------------
 
-	var ageSelect = "";
-	var ageQuery = "&age="; // age = age + "adult"
-	var ageID = "";
+	//Function to run geolocator
 
-	var sexSelect = "";
-	var sexQuery = "&sex="; // sex = sex + "F"
-	var sexID = "";
+	function Geolocator() {
 
-	var breedSelect = "";
-	var breedQuery = "&breed="; // needs further investigation
-	var breedID = "";
-
-	var sizeSelect = "";
-	var sizeQuery = "&size="; // size = size + "S"; also "M" "L" & "XL"
-	var sizeID = "";
-		
-	var queryURL = AnimURL + key + locationQuery + catDogQuery  + catDogSelect + ageQuery + ageSelect + sexQuery + sexSelect + breedQuery + breedSelect + sizeQuery + sizeSelect;
-	queryURL = queryURL.replace(" ", "+");
-
-	console.log(queryURL);
-
-		$.ajax({
-			url: queryURL,
-			dataType: "jsonp",
-			method: "GET"
-		}).done(function(response) {
-
-			var index = Math.floor((Math.random() * 24) + 0);
-			console.log("random index: " + index);
-
-			console.log(response);			
-// getting animal location
-			var respAbrev = response.petfinder.pets.pet;
-			var addr = respAbrev[index].contact.address1.$t;
-			var city = respAbrev[index].contact.city.$t;
-			var state = respAbrev[index].contact.state.$t;
-			var zipID = respAbrev[index].contact.zip.$t;
-// format animal location into standard format
-
-			locationID = addr + ", " + city + ", "  + state + zipID;
-			// locationID = locationID.replace('"', " "); 
-			var nameID = respAbrev[index].name.$t;
-			displayNameHTML(nameID);			
-
-			// console.log("locationID: " + locationID);
-			// displayAnimHTML(locationID);
-
-			// ageID = respAbrev[index].age.$t;
-			// console.log(ageID);
-			// displayAnimHTML(ageID);
-
-			// catDogAnim = respAbrev[index].animal.$t;
-			// console.log(catDogAnim);
-			// displayAnimHTML(catDogAnim);
-
-			// sexID = respAbrev[index].sex.$t;
-			// displayAnimHTML(sexID);
-
-			if (Array.isArray(respAbrev[index].breeds.breed)) {
-				breedID = respAbrev[index].breeds.breed[0].$t + "/ " + respAbrev[index].breeds.breed[1].$t + " mix";
-					
-			} else
-			 { breedID = respAbrev[index].breeds.breed.$t };
-
-			// displayAnimHTML(breedID);
-			// console.log("breedID: " + breedID);			
-
-			// sizeID = respAbrev[index].size.$t;
-			// displayAnimHTML(sizeID);
-
-			var photo = respAbrev[index].media.photos.photo[2].$t;
-
-			console.log(photo);
-
-			$('#dogimage').attr('src', photo);
-
-			$('#view-map').show();
-
-
-
-
-
-			// $('#picture').attr('src', newImg);
-
-		})
-
-		function displayNameHTML(feature) {
-			$('#petName').html(feature);
-		}
-
-		$("#view-map").on("click", function() {
-
-		var mapURL = "https://maps.googleapis.com/maps/api/geocode/json?address="
-
-		var mapAddress = locationID;
-
-		mapURL = mapURL + mapAddress;
-		mapURL = mapURL.replace(/ /g, "+");
-
-
-		console.log("mapuURL: " + mapURL);
-
-		$.ajax({
-			url: mapURL,
-			method: "GET"
-		}).done(function(response) {
-
-			console.log(response);
-
-			var responseLoc = response.results[0].geometry.location;
-			locLat = responseLoc.lat;
-			locLong = responseLoc.lng;
-
-			console.log(locLat);
-			console.log(locLong);
-
-			console.log("long&lat: " + locLat + ", " + locLong);
-
-        	var markerOptions = {
-    			position: new google.maps.LatLng(locLat, locLong)
-				};
-				var marker = new google.maps.Marker(markerOptions);
-				marker.setMap(map);
-
-
-		});
-
-		var mapOptions = {
-			panControl: false,
-			zoomControl: false
-		}
-
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 41.4931, lng: -81.6790},
-          zoom: 11,
-          mapOptions
-
-        });
-
-        infoWindow = new google.maps.InfoWindow;
+		 infoWindow = new google.maps.InfoWindow;
 
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
@@ -166,11 +81,23 @@ $(document).ready(function() {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-
+            globalLat = pos.lat;
+            globalLng = pos.lng;
+            //making markers. should turn into a function
+            var markerOptions = {
+                position: new google.maps.LatLng(pos.lat, pos.lng)
+                };
+                var marker = new google.maps.Marker(markerOptions);
+                marker.setMap(map);
+            // console.log("pos.long&lat: " + pos.lat + ", " + pos.lng);
+            
+        
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
+            // infoWindow.open(map);
+            // map.setCenter(pos);
+        ReverseGeoLocator();
+        // console.log("ZIP1: " + zip);
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
           });
@@ -178,40 +105,274 @@ $(document).ready(function() {
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
+        // console.log("begin reverse geolocator")
+            }
+     //Function to Make the Map
+     function MakeMap() {
+        // map = new google.maps.Map(document.getElementById('map'), {
+      //     center: {lat: parseInt(globalLat), lng: parseInt(globalLng)}, //lat: 41.4931, lng: -81.6790
+      //     zoom: 11,
+      //     scrollwheel: false
+      //     });
+        // console.log(mapURL);
+        var mapURLloc = mapURL + mapAddress;
+        mapURLloc = mapURLloc.replace(/ /g, "+");
+        // console.log("mapURL: " + mapURLloc);
+        $.ajax({
+            url: mapURLloc,
+            method: "GET"
+        }).done(function(response) {
+            // console.log(response);
+            var responseLoc = response.results[0].geometry.location;
+            animLocLat = responseLoc.lat;
+            animnLocLong = responseLoc.lng;
+            // console.log("lat&long: " + reanimLocLat + ", " + animnLocLong);
+            MarkerMaker(animLocLat, animnLocLong);
+            // console.log('to marker maker data: ' + globalLat + globalLng);
+            MarkerMaker(globalLat, globalLng);
+            
+        });
+        
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 41.4931, lng: -81.6790}, //lat: 41.4931, lng: -81.6790
+          zoom: 11,
+          scrollwheel: false
+        });
+        infoWindow = new google.maps.InfoWindow;
+     }
+     function MarkerMaker(lats, longs) {
+        var markerOptions = {
+                position: new google.maps.LatLng(lats, longs)
+                };
+                var marker = new google.maps.Marker(markerOptions);
+                marker.setMap(map);
+            // console.log("marker maker long&lat: " + lats + ", " + longs);
+     }
+     function DisplayAnimal() {
+// variables for user input and petfinder api output
+    // console.log("ZIP2: " + zip);
+    randZipArray = makeZipArray(44143);
 
-
-      		})
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    var randomZip = randZipArray[Math.floor(Math.random()*24 + 0)]
+    var locationQuery = "&location=" + randomZip;
+    console.log("Randomized zip locationQuery: " + locationQuery)
+   
+    loop = false;
+        
+    var queryURL = AnimURL + key + locationQuery + "&animal="  + catDogSelect + "&age=" + ageSelect + "&sex=" + sexSelect + "&breed=" + breedSelect + "&size=" + sizeSelect;
+    queryURL = queryURL.replace(" ", "+");
+        console.log("while Loop outside function");
+        AnimalAJAX(queryURL);
+    
+   
+        
+     }
+     function displayAnimHTML(feature) {
+            $('.text').append('<br>' + feature);
+        }
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(browserHasGeolocation ?
                               'Error: The Geolocation service failed.' :
                               'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);
       }
+    
+      function ReverseGeoLocator(){
+        // console.log("globalLat within reverse geolocator: " + globalLat);
+        // console.log("globalLng within reverse geolocator: " + globalLng);
+        // reverse geolocation
+            var revGeolocQuery = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + globalLat + "," + globalLng;
+            revGeolocQuery = revGeolocQuery + "&sensor=true";
+          // mapURLzip = mapURLzip.replace(/ /g, "+");
+          $.ajax({
+            url: revGeolocQuery,
+            method: "GET"
+            }).done(function(response) {
+            var searchKey = "postal_code";
+            var zipcodeObject = null;
+            for( var i = 0; i < response.results[0].address_components.length; i++ ){
+                var thisAddressObject = response.results[0].address_components[i];
+                var addressTypes = thisAddressObject.types;
+                var search = addressTypes.indexOf(searchKey);
+                if( search > -1 ){
+                    zipcodeObject = thisAddressObject;
+                    // console.log("ZIP CODE OBJECT: " + JSON.stringify(zipcodeObject.short_name));
+                    zip = zipcodeObject.short_name;
+                    // console.log("ZIP3: " + zip);
+                    break;
+                }
+            }
+            
+            if( zipcodeObject !== null ){
+                // console.log(zipcodeObject);
+            }
+        });
+            
+      };
+      function makeZipArray(number) {
+        number = parseInt(number);
+        var posIterator = number + 13;
+        var negIterator = number - 13;
+        var Ziparray = [];
+        for (var i = number; i < posIterator; i++) {
+            Ziparray.push(i);
+        }
+        for (var i = number - 1; i > negIterator; i--) {
+            Ziparray.push(i);
+        }
+        Ziparray.map(String);
+                    // console.log(Ziparray);
+        return Ziparray;
+      }
+      function AnimalAJAX(queryURL) {
+            $.ajax({
+            url: queryURL,
+            dataType: "jsonp",
+            method: "GET"
+        }).done(function(response) {
+            console.log("while loop")
+            var index = Math.floor((Math.random() * 24) + 0);
+            // console.log("random index: " + index);
+            if (response.petfinder.pets == undefined) {
+                randomZip = randZipArray[Math.floor(Math.random()*24 + 0)]
+                locationQuery = "&location=" + randomZip;
+                queryURL = AnimURL + key + locationQuery + "&animal="  + catDogSelect + "&age=" + ageSelect + "&sex=" + sexSelect + "&breed=" + breedSelect + "&size=" + sizeSelect;
+                queryURL = queryURL.replace(" ", "+");
+                return AnimalAJAX(queryURL);
+            } 
+            loop = true;
+            
+            // console.log(response);           
+// getting animal location
+            respAbrev = response.petfinder.pets.pet;
+            console.log(respAbrev);
+            addr = respAbrev[index].contact.address1.$t;
+            city = respAbrev[index].contact.city.$t;
+            state = respAbrev[index].contact.state.$t;
+            zipID = respAbrev[index].contact.zip.$t;
+// format animal location into standard format
+            locationID = addr + ", " + city + ", "  + state + " " + zipID;
+            mapAddress = locationID;
+            console.log("locationID check undf: " + locationID);
+            if (locationID.indexOf('undefined') == 0) {
+            locationID = locationID.replace(/undefined, /g, "");
+            }
+
+            console.log("locationID: " + locationID);
+            // locationID = locationID.replace('"', " "); 
+            nameID = respAbrev[index].name.$t;
+            $('#petName').html(nameID);         
+            console.log("nameID: " + nameID);
+            // displayAnimHTML(locationID);
+            ageID = respAbrev[index].age.$t;
+            console.log("ageID: " + ageID);
+            // displayAnimHTML(ageID);
+            catDogAnim = respAbrev[index].animal.$t;
+            console.log("catDogAnim: " + catDogAnim);
+            // displayAnimHTML(catDogAnim);
+            sexID = respAbrev[index].sex.$t;
+            console.log("sexID: " + sexID)
+            // displayAnimHTML(sexID);
+            phoneID = respAbrev[index].contact.phone.$t;
+            console.log("phoneID: " + phoneID);
+
+            emailID = respAbrev[index].contact.email.$t;
+            // displayAnimHTML(emailID);
+            console.log("phoneID: " + phoneID);
+
+            
+            if (Array.isArray(respAbrev[index].breeds.breed)) {
+                breedID = respAbrev[index].breeds.breed[0].$t + "/ " + respAbrev[index].breeds.breed[1].$t + " mix";
+                    
+            } else
+             { breedID = respAbrev[index].breeds.breed.$t };
+
+            // displayAnimHTML(breedID);
+            console.log("breedID: " + breedID); 
+
+            sizeID = respAbrev[index].size.$t;
+
+            // displayAnimHTML(sizeID);
+            photo = respAbrev[index].media.photos.photo[2].$t;
+            console.log("PHOTO-1: " + photo);
+            $('#dogimage').attr('src', photo);// corey change here
+            // $('#map').show();
+
+                    // FireBaseFunction();
 
         })
 
-// function mapAPI() {
-// 		//map api
+        
+    }
 
-// 		var mapURL = "https://maps.googleapis.com/maps/api/geocode/json?address="
+    function FireBaseFunction() {
 
-// 		var mapAddress = locationID;
+    	 event.preventDefault();
 
-// 		mapAddress = mapURL + locationID;
+    	  sexID = FixUndefined(sexID);
+    	  nameID = FixUndefined(nameID);
+    	  ageID = FixUndefined(ageID);
+    	  catDogAnim = FixUndefined(catDogAnim);
+    	  breedID = FixUndefined(breedID);
+    	  locationID = FixUndefined(locationID);
+    	  phoneID = FixUndefined(phoneID);
+    	  emailID = FixUndefined(emailID);
+    	  photo = FixUndefined(photo);
 
-// 		console.log("mapuURL: " + mapURL);
+    	  console.log("FB " + sexID);
+    	  console.log("FB " + nameID);
+    	  console.log("FB " + ageID);
+    	  console.log("FB " + catDogAnim);
+    	  console.log("FB " + breedID);
+    	  console.log("FB " + locationID);
+    	  console.log("FB " + phoneID);
+    	  console.log("FB " + emailID);
+    	  console.log("FB " + photo);
 
-// 		var mapQueryURL 
-// 		// var mapOptions = {
-//   //   		center: new google.maps.LatLng(41.4931,-81.6790),
-//   //  			zoom: 12,
-//   //   		mapTypeId: google.maps.MapTypeId.ROADMAP
-// 		// };
+            
+            //var zipInput = $("#zip").val();
+            var userZip = '&location=' + zip;
+            var pfApiUrl = "http://api.petfinder.com/pet.find?format=json&key=f411ff9725d287d4138503a0c95030a6&count=1&output=basic"
+            var queryURL = pfApiUrl + userZip;
+            console.log(queryURL);
+            $.ajax({
+                url: queryURL,
+                dataType: "jsonp",
+                method: "GET"
+            }).done(function(response) {
+                console.log(response);
+                console.log("PHOTO: " + photo);
+                var petInfo = {
+                animal: catDogAnim,
+                breed: breedID,
+                sex: sexID,
+                name: nameID,
+                age: ageID,
+                address: locationID,
+                // city: city,
+                // state: state,
+                // zip: zipID,
+                phone: phoneID,
+                email: emailID,
+                photo: photo,
+                animLocLat: animLocLat,
+                animnLocLong: animnLocLong
+                }
+                console.log("FIREBASE" + petInfo);
+                    database.ref().push(petInfo);
+            });
+    }
 
-// 		new google.maps.Map(document.getElementById('map'), mapOptions);
-// 	}
-
-	
+    function FixUndefined(variable) {
+    	console.log(variable);
+    	 if (variable === undefined || variable === null) {
+            var returnVar = "No information";
+            return returnVar;
+            } else {
+            	return variable;
+            }
+    }
+     
 });
+
